cead469289e7ca9362d4dae838aceb681cd642fa
Fasquini <fasquini@udesa.edu.ar>
2026-06-04 23:29:42 -0300

    Agregado de nueva tab a la seccion Home

--- demo-expo/src/components/HomeMenu/HomeMenu.js
+++ demo-expo/src/components/HomeMenu/HomeMenu.js
@@ -1,19 +1,27 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import Feather from '@expo/vector-icons/Feather';
 import Home from '../../screens/Home/Home';
 import User from '../../screens/User/User';
+import Post from '../../screens/Post/Post';
 const Tab = createBottomTabNavigator()
 function HomeMenu(props){
     return(
         <Tab.Navigator screenOptions={{tabBarShowLabel:false}}>
             <Tab.Screen name='Home' component={Home} options={
-                {
-                     tabBarIcon: ()=> <AntDesign name="home" size={24} color="black" />
+                {    headerShown:false,
+                     tabBarIcon: ()=> <Feather  name="home" size={24} color="black" marginTop="15px" />
+                }
+                }/>
+            <Tab.Screen name='Post' component={Post} options={
+                {    headerShown:false,
+                     tabBarIcon: ()=> <Feather name="plus-circle" size={24} color="black" weight = "700"/>
+
                 }
                 }/>
             <Tab.Screen name='Profile' component={User}options={
-                {
-                    tabBarIcon: ()=> <AntDesign name="user" size={24} color="black" />
+                {   headerShown:false,
+                    tabBarIcon: ()=> <Feather name="user" size={24} color="black" marginTop="15px" />
                 }
                 }/>
         </Tab.Navigator>
---
+++ demo-expo/src/screens/Post/Post.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View, Pressable} from "react-native";
+import { StyleSheet } from "react-native";
+function Post (props){
+    return(
+    <View>
+      <Text> Sección para añadir posteos </Text> 
+    </View>
+    )
+}
+export default Post;
